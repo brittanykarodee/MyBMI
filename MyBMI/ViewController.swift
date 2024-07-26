aef317e34e168614036c6806a61c32868e12bafe
Brittany Angelle <b@wearetech.education>
2024-07-26 17:56:11 -0400

    Initial Commit

---
+++ MyBMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBMI
+//
+//  Created by Brittany P Jenkins on 7/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
